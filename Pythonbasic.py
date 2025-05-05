f129972dbea91ce36ba015f884f4708d42f93ed2
Nixon Antony <nixes.de313antony@gmail.com>
2025-05-05 20:44:34 +0200

    py basic

---
+++ Pythonbasic.py
@@ -0,0 +1,27 @@
+# print function print("")
+# print("hallo world")
+
+# variable
+# var1=12
+# print(var1)
+# var=6
+# Var=12
+# print(Var)
+# print(var)
+# a=12
+# print(a)
+# print(type(a))
+
+# operators (arithmetic operators in python)
+# sum=3+9
+# print(sum)
+
+# a=5
+# b=15
+# sum=a+b
+# print(sum)
+
+a=5
+b=15
+sum=a+b
+print("sum of",a,"+",b,"=",sum)
